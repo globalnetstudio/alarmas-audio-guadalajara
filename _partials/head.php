b39fbe451b1cf5b76a6e1a5f4a69a3843844be97
Daniel González Briseño <Daniel Gonzlez Briseo>
2016-04-21 17:48:49 -0500

    Se agregó datos de contacto e iconos superiores

--- _partials/head.php
+++ _partials/head.php
@@ -11,6 +11,7 @@
 
 		<!-- Styles  -->
 		<link rel="stylesheet" href="./css/reset.css">
+		<link rel="stylesheet" href="./css/fontastic.css">
 		<link rel="stylesheet" href="./css/styles.css">
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- index.php
+++ index.php
@@ -8,8 +8,10 @@
 		<div class="left-background"></div>
 		<div class="middle-background"></div>
 		<div class="right-background"></div>
-		<div class="logo"><img src="/images/alarmas-audio-guadalajara-logo.png" alt=""></div>	
+		<div class="logo"><img src="/images/alarmas-audio-guadalajara-logo.png" alt=""></div>
+
 		<nav>
+			<!-- .main-menu -->
 			<div class="main-menu">
 				<ul>
 					<li class="active"><a href="#">Inicio</a></li>
@@ -19,15 +21,70 @@
 					<li><a href="#contacto">Contacto</a></li>
 				</ul>
 			</div>
+			<!-- /.main-menu -->
 		</nav>
-		<div class="address">Direcciones</div>
-		<div class="social-icons">Iconos Redes Sociales</div>
+
+		<!-- .address -->
+		<div class="address">
+			<!-- .left-column -->
+			<div class="left-column">
+				<p class="address-1">
+					<i class="icon-mark"></i>
+					Agustín Yáñez #2256-A, Arcos Sur, CP. 44150, Guadalajara, Jal.
+				</p>
+				<p class="address-2">
+					<i class="icon-mark"></i>
+					Av Patria #440-D, La Estancia, CP. 45030 Zapopan, Jal.
+				</p>
+				<p class="email">
+					<i class="icon-mail"></i>
+					contacto@alarmasyaudioguadalajara.com
+				</p>
+			</div>
+			<!-- /.left-column -->
+
+			<!-- .right-column -->
+			<div class="right-column">
+				<p class="phone-1"><i class="icon-phone"></i>(33) 3616 - 9088</p>
+				<p class="phone-2"><i class="icon-phone"></i>(33) 3070 - 0576</p>
+				<div class="social-icons">
+					<!-- .twitter -->
+					<div class="twitter">
+						<a href="#"><i class="icon-twitter"></i></a>
+					</div>
+					<!-- /.twitter -->
+
+					<!-- .facebook -->
+					<div class="facebook">
+						<a href="#"><i class="icon-facebook"></i></a>
+					</div>
+					<!-- /.facebook -->
+
+					<!-- .youtube -->
+					<div class="youtube">
+						<a href="#"><i class="icon-youtube"></i></a>
+					</div>
+					<!-- /.youtube -->
+
+					<!-- .google-plus -->
+					<div class="google-plus">
+						<a href="#"><i class="icon-google-plus"></i></a>
+					</div>
+					<!-- /.google-plus -->
+				</div>
+			</div>
+			<!-- /.right-column -->
+		</div>
+		<!-- /.address -->
 	</header>
 
+	<!-- .carousel -->
 	<div class="carousel">
 		<img src="/images/header/banner/slide-1.jpg" alt="Slider Banner 1">
-	</div>	
+	</div>
+	<!-- /.carousel -->
 
+	<!-- .content -->
 	<section class="content">
 		<div class="security-comfort">
 			Seguridad y Confort
@@ -42,5 +99,6 @@
 			Alarmas Viper Clifford
 		</div>
 	</section>
+	<!-- /.content -->
 
 <?php include_once('./_partials/footer.php'); ?>	
