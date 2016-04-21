ca7ddfbea818c2e8c68f40c348ef55be84b25d06
Daniel González Briseño <Daniel Gonzlez Briseo>
2016-04-21 04:25:25 -0500

    Cambios en el header
    -- Se hizo el menu principal
    -- Se creó el slider de fondo
    -- Se posicionó el logo

--- _partials/footer.php
+++ _partials/footer.php
@@ -1,3 +1,8 @@
+		<footer class="footer">
+			<p class="copyright">Copyright 2016 &copy; Alarmas &amp; Audio &#8226; Todos los Derechos Reservados</p>
+			<p class="created-by">Desarrollado por <img class="global-net-studio-logo" src="/images/icons/global-net-studio-logo.png" alt=""> Global Net Studio</p>
+		</footer>
+
 		<!-- jQuery -->
 		<script src="./js/jquery.min.js"></script>
 	</body>
--- index.php
+++ index.php
@@ -4,4 +4,43 @@
 
 	<h1 class="hide">Alarmas y Audio Guadalajara</h1>
 
+	<header class="header">		
+		<div class="left-background"></div>
+		<div class="middle-background"></div>
+		<div class="right-background"></div>
+		<div class="logo"><img src="/images/alarmas-audio-guadalajara-logo.png" alt=""></div>	
+		<nav>
+			<div class="main-menu">
+				<ul>
+					<li class="active"><a href="#">Inicio</a></li>
+					<li><a href="#alarmas">Alarmas</a></li>
+					<li><a href="#audio">Audio</a></li>
+					<li><a href="#confort">Confort</a></li>
+					<li><a href="#contacto">Contacto</a></li>
+				</ul>
+			</div>
+		</nav>
+		<div class="address">Direcciones</div>
+		<div class="social-icons">Iconos Redes Sociales</div>
+	</header>
+
+	<div class="carousel">
+		<img src="/images/header/banner/slide-1.jpg" alt="Slider Banner 1">
+	</div>	
+
+	<section class="content">
+		<div class="security-comfort">
+			Seguridad y Confort
+		</div>
+		<div class="avital-products">
+			Productos Avital
+		</div>
+		<div class="alarm-specialists">
+			Especialistas en alarmas
+		</div>
+		<div class="viper-clifford-alarms">
+			Alarmas Viper Clifford
+		</div>
+	</section>
+
 <?php include_once('./_partials/footer.php'); ?>	
