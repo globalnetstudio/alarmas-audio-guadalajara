89fa96fbdbca8c56ba395fa04e790297b0ac26ba
Daniel González Briseño <Daniel Gonzlez Briseo>
2016-04-20 22:57:40 -0500

    Primer Commit

---
+++ _partials/footer.php
@@ -0,0 +1,4 @@
+		<!-- jQuery -->
+		<script src="./js/jquery.min.js"></script>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php $pageTitle= 'Homepage'; $pageId = 'homepage'; ?>
+
+<?php include_once('./_partials/head.php'); ?>
+
+	<h1 class="hide">Alarmas y Audio Guadalajara</h1>
+
+<?php include_once('./_partials/footer.php'); ?>	
