e3597d88e771a25d3e96f32d47b0ed2ddaf48561
Daniel González Briseño <Daniel Gonzlez Briseo>
2016-04-21 19:19:39 -0500

    Se terminó el marcado del Homepage

--- _partials/head.php
+++ _partials/head.php
@@ -19,6 +19,9 @@
 	    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	  <![endif]-->
+
+		<!-- Google Fonts -->
+	  <link href='https://fonts.googleapis.com/css?family=Montserrat:700|Bowlby+One' rel='stylesheet' type='text/css'>
 	</head>
 
 	<body id="<?php echo (isset($pageId)) ? $pageId : null ?>">
--- index.php
+++ index.php
@@ -86,19 +86,62 @@
 
 	<!-- .content -->
 	<section class="content">
+		<!-- .security-comfort -->
 		<div class="security-comfort">
-			Seguridad y Confort
+			<h2>¡Obtener una nueva alarma de coche o sistema de encendido remoto nunca había sido tan fácil!</h2>
+			<p>Ahora puedes personalizar el sistema de seguridad perfecto para tu vehiculo,<br> solo dinos como lo quieres y nosotros te ayudamos.</p>
 		</div>
-		<div class="avital-products">
-			Productos Avital
+		<!-- /.security-comfort -->
+
+		<!-- .alarm-controls -->
+		<div class="alarm-controls">
+			<h2>Asesoria y Satisfacción Garantizada</h2>
+			<img class="img-responsive" src="/images/controles-clifford.jpg" alt="Controles para alarmas Clifford">
 		</div>
-		<div class="alarm-specialists">
-			Especialistas en alarmas
-		</div>
-		<div class="viper-clifford-alarms">
-			Alarmas Viper Clifford
-		</div>
+		<!-- /.alarm-controls -->
+
 	</section>
 	<!-- /.content -->
+
+	<!-- .alarms-audio-confort -->
+	<div class="alarms-audio-confort">
+		<img class="img-responsive" src="/images/alarms-audio-confort.jpg" alt="Alarmas Audio y Confort">
+	</div>
+	<!-- /.alarms-audio-confort -->
+
+	<!-- .avital-products -->
+	<div class="avital-products">
+		<img class="img-responsive" src="/images/avital-products.jpg" alt="Productos Avital">
+	</div>
+	<!-- /.avital-products -->
+
+	<!-- .alarms-specialists -->
+	<div class="alarms-specialists">
+		<h2 class="hide">!Especialistas en Instalacion de Alarmas!</h2>
+		<p class="hide">Contamos con el personal capacitado en las tecnologías de punta de sistemas de seguridad y confort automotriz.</p>
+		<img class="img-responsive" src="/images/alarms-specialists.jpg" alt="Productos Avital">
+	</div>
+	<!-- /.alarms-specialists -->
+
+	<!-- .viper-clifford-features -->
+	<div class="viper-clifford-features">
+		<!-- .wrapper -->
+		<div class="wrapper">			
+			<!-- .clifford-features -->
+			<div class="clifford-features">
+				<img class="img-responsive" src="/images/viper-clifford-features.jpg" alt="Viper Clifford Ventajas">
+			</div>
+			<!-- /.clifford-features -->
+
+			<!-- .text -->
+			<div class="text">
+				<h2>¡Alarmas Viper/Clifford!</h2>
+				<p>Son los sistemas de seguridad y arranque remoto para   vehículos más vendido del mundo. Utilizan tecnología de punta para estar seguros de que usted tiene el control,  proporcionando mayor alcance y las características con las que usted puede contar en todo momento.</p>
+			</div>
+			<!-- /.text -->
+		</div>
+		<!-- /.wrapper -->
+	</div>
+	<!-- /.viper-clifford-features -->
 
 <?php include_once('./_partials/footer.php'); ?>	
