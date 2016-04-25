a960a94b4ef8c035eb7de2a7ea8b16a62602cff4
Daniel González Briseño <Daniel Gonzlez Briseo>
2016-04-25 16:24:17 -0500

    Se terminó la sección de audio

--- audio.php
+++ audio.php
@@ -8,10 +8,60 @@
 
 	<?php include('./_partials/carousel.php'); ?>
 
-	<!-- .content -->
-	<section class="content">
+	<!-- .audio-content -->
+	<section class="audio-content">
 
+		<div class="row">
+			<div class="column">
+				<img src="/images/audio/pioneer-audio.jpg" alt="Pioneer Audio">
+			</div>
+			<div class="column">
+				<img src="/images/audio/mtx-audio.jpg" alt="MTX Audio">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="column">
+				<img src="/images/audio/jbl-audio.jpg" alt="JBL Audio">
+			</div>
+			<div class="column">
+				<img src="/images/audio/clarion-audio.jpg" alt="Clarion Audio">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="column">
+				<img src="/images/audio/audison-audio.jpg" alt="Audison Audio">
+			</div>
+			<div class="column">
+				<img src="/images/audio/hertz-audio.jpg" alt="Hertz Audio">
+			</div>
+		</div>
+
+		<div class="directed-logo">
+			<img src="/images/audio/directed-logo.jpg" alt="Directed Logo">
+		</div>
+
+		<div class="row">
+			<div class="column">
+				<img src="/images/audio/directed-car-screen.jpg" alt="Directed Car Screen">
+			</div>
+			<div class="column">
+				<img src="/images/audio/directed-car-screen-2.jpg" alt="Directed Car Screen">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="column">
+				<img src="/images/audio/directed-tablet.jpg" alt="Directed Tablet">
+			</div>
+			<div class="column">
+				<img src="/images/audio/directed-touch-menu.jpg" alt="Directed Touch Menu">
+			</div>
+		</div>
+
+		<p class="information">Con <b>ASTEROID</b> Smart entrará en una nueva dimensión del entretenimiento y de la ayuda a la conducción. Igual que con su tableta digital o Smartphone, personalice su <b>ASTEROID</b> Smart con aplicaciones, acceda a Internet y navegue por GPS.</p>
 	</div>
-	<!-- /.content -->
+	<!-- /.audio-content -->
 
 <?php include_once('./_partials/footer.php'); ?>	
