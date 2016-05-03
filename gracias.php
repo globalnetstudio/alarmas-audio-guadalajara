bd80c339d78523dbb1f86704935f59be365b10c9
Daniel González Briseño <sonusbeat@hotmail.com>
2016-05-02 22:10:19 -0500

    Se quito el carrusel en gracias

--- gracias.php
+++ gracias.php
@@ -1,4 +1,5 @@
-<?php session_start();?>
+<?php session_start(); $page= "thanks"; ?>
+
 <?php if(isset($_SESSION['mail-sended']) && $_SESSION['mail-sended'] == true) : ?>
 
 	<?php include('./_partials/head.php'); ?>
@@ -6,8 +7,6 @@
 		<h1 class="hide">Alarmas y Audio Guadalajara</h1>
 	
 		<?php include('./_partials/header.php'); ?>
-	
-		<?php include('./_partials/carousel.php'); ?>
 	
 		<!-- .thanks-content -->
 		<section class="thanks-content">
